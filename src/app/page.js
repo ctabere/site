f00334bfbbb3db41ecd4880e9146d0c62938e50e
MetaMelchoir <ctabere@users.noreply.github.com>
2024-07-04 10:57:36 -0400

    NavBar

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="bg-black text-white">
+    <main className="bg-orange-100 text-blue-800 min-h-screen flex flex-col items-center justify-center">
       <p>Manchester Kum Sung</p>
     </main>
   );
