ed401fbdb53add0a74eac910c792dfcac49d0752
MetaMelchoir <ctabere@users.noreply.github.com>
2024-07-07 23:53:07 -0400

    navbar broken

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
+'use client'
+
+import React from "react";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="bg-orange-100 text-blue-800 min-h-screen flex flex-col items-center justify-center">
-      <p>Manchester Kum Sung</p>
+      <h1 className="text-6xl font-bold text-accent">Manchester Kum Sung</h1>
     </main>
   );
 }
